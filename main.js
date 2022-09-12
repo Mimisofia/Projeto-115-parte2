3663f1726cd132c19a7db8d9c3ac62985bfb9c92
Mimisofia <99193753+Mimisofia@users.noreply.github.com>
2022-09-12 10:46:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX=0;
+noseY=0;
+function preload() {
+  clownNose = loadImage('https://i.postimg.cc/JhCypSw2/kisspng-sunglasses-drawing-vector-hand-painted-sunglasses-5a88e25b685f29-5898265415189202834275-remo.png');
+}
+
+function setup(){
+ canvas = createCanvas(300, 300);
+ canvas.center();
+ video= createCapture(VIDEO);
+ video.size(300, 300);
+ video.hide();
+
+ poseNet = ml5.poseNet(video, modelLoaded);
+ poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+  console.log('PoseNet foi inicializado');
+}
+
+function gotPoses(results){
+ if(results.length > 0)
+ {
+  console.log(results);
+  noseX = results[0].pose.nose.x -50;
+  noseY = results[0].pose.nose.y -78;
+ }
+}
+
+function draw()
+{
+  image(video, 0, 0, 300, 300);
+  image(clownNose, noseX, noseY, 90, 90);
+}
+
+function takeSnapshot(){
+  save('myImage.png');
+}
